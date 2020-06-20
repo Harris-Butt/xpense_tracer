3d18c7770a1ca746994764e2032f358224e04c66
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2020-06-21 01:50:30 +0500

    Finish iteration 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Grid} from '@material-ui/core'
+import HeaderComponent from './Components/HeaderComponent'
+import TotalCreditComponent from './Components/TotalCreditComponent'
+import IncomeAndExpenseComponent from './Components/IncomeAndExpenseComponent'
+import TransactionHistroryListComponent  from './Components/TransactionHistoryListComponent'
+import NewTransactionComponent from './Components/NewTransactionComponent'
 function App() {
   return (
-    <div className="App">
-      <h1>React Bootstrap </h1>
-    </div>
+    <>
+      <Grid container direction ="column" spacing={1} align= "center">
+        <Grid  item >
+          <Grid  xs={0} sm={4}></Grid>
+          <Grid  xs={12} sm={4} align="center"><HeaderComponent /></Grid>
+          <Grid  xs={0} sm={4}></Grid>
+        </Grid>
+        <Grid item>
+          <Grid  xs={0} sm={4}></Grid>
+          <Grid  xs={12} sm={4} align="center"><TotalCreditComponent /></Grid>
+          <Grid  xs={0} sm={4}></Grid>
+        </Grid>
+      </Grid>
+      <Grid container direction ="row" >
+        <Grid  xs={0} sm={3}></Grid>
+          <IncomeAndExpenseComponent/>
+        <Grid  xs={0} sm={3}></Grid>
+      </Grid>
+      <br />
+      <Grid container direction="row">
+          <Grid  item xs={0} sm={4}></Grid>
+          <Grid  item xs={12} sm={4}><TransactionHistroryListComponent /></Grid>
+          <Grid  item xs={0} sm={4}></Grid>
+      </Grid>
+      <br />
+      <Grid container direction="row">
+          <Grid  item xs={0} sm={4}></Grid>
+          <Grid  item xs={12} sm={4}><NewTransactionComponent/></Grid>
+          <Grid  item xs={0} sm={4}></Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/Components/NewTransactionComponent.js
@@ -0,0 +1,90 @@
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Paper } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+const  NewTransactionComponent = () => {
+
+  const classes = useStyles();
+  return (
+      <>
+      <Paper>
+        <Typography gutterBottom variant="h5" component="h2">Add new Transaction</Typography>
+      
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="description"
+            label="Description"
+            name="description"
+            autoComplete="description"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="amount"
+            label="Amount"
+            type="number"
+            id="amount"
+            autoComplete="Amount"
+          />
+        
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Add new Transaction
+          </Button>
+        
+        </form>
+       
+      
+    </Container>
+    </Paper>
+    </>
+
+  )
+}
+export default NewTransactionComponent;
